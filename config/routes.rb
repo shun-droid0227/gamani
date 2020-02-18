0e68be631fa0990d9fe7acd57a921461356d053b
shun-droid0227 <thermohygro1991@gmail.com>
2020-02-18 11:10:55 +0000

    devise install

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+end
