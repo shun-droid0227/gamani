3b86be95c7153230b16cb782be4dac7356054c24
shun-droid0227 <thermohygro1991@gmail.com>
2020-02-19 07:22:34 +0000

    add user create,edit, post create,delete function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    user_path(resource, method: :get) # ログイン後に遷移するpath
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpath
+  end
+
 end
