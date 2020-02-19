32a67b6aa291cb18e27851df7d473a09b360aa88
shun-droid0227 <thermohygro1991@gmail.com>
2020-02-19 19:00:31 +0000

    add create game_tittle function

--- app/controllers/game_tittles_controller.rb
+++ app/controllers/game_tittles_controller.rb
@@ -1,2 +1,22 @@
 class GameTittlesController < ApplicationController
+
+  def index
+    @game_tittles = GameTittle.all
+    @new_tittle =GameTittle.new
+  end
+
+  def create
+    game_tittle = GameTittle.new(game_tittle_params)
+    game_tittle.save
+    redirect_to game_tittles_path
+  end
+
+  def destroy
+
+  end
+
+  def game_tittle_params
+    params.require(:game_tittle).permit(:name,:game_genle_id)
+  end
+
 end
