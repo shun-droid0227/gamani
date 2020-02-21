aa2724102b4cd05e645aeed0d8f06ad1c01a9841
shun-droid0227 <thermohygro1991@gmail.com>
2020-02-21 17:20:10 +0000

    add post favorite create&destroy action

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_150630) do
+ActiveRecord::Schema.define(version: 2020_02_21_130157) do
 
   create_table "chat_threads", force: :cascade do |t|
     t.string "name"
     t.integer "game_tittle_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
