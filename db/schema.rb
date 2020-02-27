87ae2b16411c9d9bac6fb1c0a49adbc647006691
shun-droid0227 <thermohygro1991@gmail.com>
2020-02-27 10:06:27 +0000

    user_edit fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_170421) do
+ActiveRecord::Schema.define(version: 2020_02_27_085835) do
 
   create_table "chat_threads", force: :cascade do |t|
     t.string "name"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2020_02_26_170421) do
     t.string "name"
     t.string "introduction"
     t.string "image_id"
+    t.string "roll_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
