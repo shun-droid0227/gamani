8c8c7762577f78c7ba7e8d552cadfb998b89d68e
shun-droid0227 <thermohygro1991@gmail.com>
2020-03-03 10:21:04 +0000

    layout fix

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,25 +1,25 @@
 class PostCommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     post_comment = PostComment.new(post_comment_params)
     post_comment.user_id = current_user.id
-    post_comment.post_id = post.id
+    post_comment.post_id = @post.id
     post_comment.save(post_comment_params)
     @post_comment = PostComment.new
   end
 
   def destroy
     post_comment = PostComment.find(params[:id])
-    @post = Post.find(params[id: post_comment.post_id])
     post_comment.destroy
-    @post_comments = PostComment.new
+    @post = Post.find(post_comment.post_id)
+    @post_comment = PostComment.new
   end
 
   private
 
   def post_comment_params
-    params.require(:post_comment).permit(:user_id,:post_id,:image,:sentence)
+    params.require(:post_comment).permit(:sentence)
   end
 
 end
